b125f5c0682b77ae944f93364ccdd14a3e387f65
Omkar <omkarj6108@gmail.com>
2025-12-23 20:31:06 +0530

    third commit

--- for_loop_q5.py
+++ for_loop_q5.py
@@ -1,4 +1,11 @@
-#print all even numbers from 1 to 100
+#print all even and odd numbers from 1 to 25
 
-for i in range(2,102,2):
+print("Even Numbers are : \n")
+for i in range(2,26,2):
+    print(i)
+
+print("\n\n")
+
+print("Odd Numbers are : \n")
+for i in range(1,26,2):
     print(i)
