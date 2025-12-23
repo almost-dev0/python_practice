cf16c0cfeeb2555c5d392874b7fe7e50ad42343b
Omkar <omkarj6108@gmail.com>
2025-12-23 17:51:26 +0530

    third commit

---
+++ for_loop_q1.py
@@ -0,0 +1,6 @@
+#print element of given list using for loop fruits = ["apple","banana","watermelon","strawberry","mango"]
+
+fruits = ["apple","banana","watermelon","strawberry","mango"]
+
+for val in fruits : 
+    print(val)
---
+++ for_loop_q2.py
@@ -0,0 +1,9 @@
+#take string input from user and print all vowels
+
+str = input("Enter any word or sentence : ")
+
+for char in str : 
+    if (char == "a" or char == "e" or char == "i" or char == "o" or char == "u"):
+        print(char)
+    elif (char == "A" or char == "E" or char == "I" or char == "O" or char == "U"):
+        print(char)
